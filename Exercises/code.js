5d6cbedad0837ee2a17117fe425a76a0937c032c
gb1083 <gabriel.burt@unh.edu>
2026-02-09 16:14:48 +0000

    Adding participation exercies

---
+++ Exercises/code.js
@@ -0,0 +1,12 @@
+console.log("Hello")
+//Part 1
+let start = Date.now();
+setTimeout(()=> {
+    console.log("Timeout ran at",
+        Date.now() - start);
+
+},20);
+while (Date.now() < start +50) {}
+console.log("Wasted time until",
+    Date.now() - start);
+
